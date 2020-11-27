4540ec36a75dc86be5c7af717e2a68e269ac6cdb
msalerno93 <msalerno1993@gmail.com>
2020-11-27 00:20:17 -0500

    finished the scrapper

--- lib/Newegg_Scraper/cli.rb
+++ lib/Newegg_Scraper/cli.rb
@@ -3,43 +3,56 @@ class NeweggScraper::CLI
     def call
         puts "Below is a list of all the new Ryzen CPUs from AMD!"
         puts "Which Ryzen CPU would you like to learn more about?"
-        # cpu_list
+        cpu_list
         interface
     end
 
     def cpu_list
-        @cpu = NeweggScraper::CPU.all
+        puts "1. AMD Ryzen 9 5950X"
+        puts "2. AMD Ryzen 9 5900X"
+        puts "3. AMD Ryzen 7 5800X"
+        puts "4. AMD Ryzen 5 5600X"
     end
 
     def interface
+        menu = "\nEnter 'list' to see the full list or enter another number"
+        puts "Please select a number from the above list or type exit."
         while true
-            puts "Please select a number from the above list or type exit."
+            
             input = gets.strip
             case input
             when "1"
-                ryzen_9 = NeweggScraper::Scraper.get_cpu_info("1")
-                description(ryzen_9)
+                cpu = NeweggScraper::Scraper.get_cpu_info("1")
+                puts description(cpu)
+                puts menu
             when "2"
-                puts "More info 2"
+                cpu = NeweggScraper::Scraper.get_cpu_info("2")
+                puts description(cpu)
+                puts menu
             when "3"
-                puts "More info 3"
+                cpu = NeweggScraper::Scraper.get_cpu_info("3")
+                puts description(cpu)
+                puts menu
             when "4"
-                puts "More info 4"
+                cpu = NeweggScraper::Scraper.get_cpu_info("4")
+                puts description(cpu)
+                puts menu
             when "exit"
                 bye
                 break
             when "list"
                 cpu_list
+                puts "Please select a number from the above list or type exit."
             else
                 puts "Incorrect command!"
+                puts "Please select a number 1-4."
             end
         end
     end
 
-    # def description(cpu)
-    #     "Name: #{cpu.name}
-
-    #     "
+    def description(thing)
+        "Name: #{thing.name}\nPrice: #{thing.price}\nFrequency: #{thing.frequency}\nCore_Count: #{thing.core_count}\nAvailability: #{thing.availability}\nURL: #{thing.url}"
+    end
 
     def bye
         puts "Thank you for using this application to learn about the new Ryzen CPUs! Come again soon!!"
--- lib/Newegg_Scraper/scraper.rb
+++ lib/Newegg_Scraper/scraper.rb
@@ -1,20 +1,26 @@
 class NeweggScraper::Scraper
 
-    @base_link = "https://www.newegg.com/"
+    @@base_link = "https://www.newegg.com/"
 
     def self.get_cpu_info(num)
         case num
         when "1"
-            link = @base_link + "amd-ryzen-9-5950x/p/N82E16819113663?&quicklink=true"
+            link = @@base_link + "amd-ryzen-9-5950x/p/N82E16819113663?&quicklink=true"
         when "2"
-            link = @base_link + "amd-ryzen-9-5900x/p/N82E16819113664?&quicklink=true"
+            link = @@base_link + "amd-ryzen-9-5900x/p/N82E16819113664?&quicklink=true"
         when "3"
-            link = @base_link + "amd-ryzen-7-5800x/p/N82E16819113665?&quicklink=true"
+            link = @@base_link + "amd-ryzen-7-5800x/p/N82E16819113665?&quicklink=true"
         when "4"
-            link = @base_link + "amd-ryzen-5-5600x/p/N82E16819113666?&quicklink=true"
+            link = @@base_link + "amd-ryzen-5-5600x/p/N82E16819113666?&quicklink=true"
         end
         doc = Nokogiri::HTML(open(link))
-        binding.pry
-    end
 
+        info = doc.css("div#product-details table.table-horizontal td").map {|thing|thing.text}
+        
+        price = doc.css("div.product-buy-box li.price-current").text
+        availablity = doc.css("div.product-buy-box div#ProductBuy").text  
+
+        cpu = NeweggScraper::CPU.new(info[3], price, info[8], info[6], link, availablity)
+        cpu
+    end
 end
